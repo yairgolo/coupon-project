4223e2a5eccbd74a2b4f92c4aba087b83714d969
yairgolo <93384953+yairgolo@users.noreply.github.com>
2022-04-03 19:35:10 +0300

    git init

---
+++ src/main/java/com/yair/couponproject/CouponProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yair.couponproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yair/couponproject/CouponProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yair.couponproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
